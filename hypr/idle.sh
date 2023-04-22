bcca2b0e171f2497f674a2a2aeef7d087a94b88d
gbj1viciado <fanaticoporjogo@hotmail.com>
2023-04-22 18:32:02 -0300

    Update configs

--- hypr/idle.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
- exec swayidle -w \
-          timeout 900 'swaylock --screenshots --effect-blur 7x5 --clock --indicator-idle-visible --fade-in 0.5 --ring-color 464653' \
-          timeout 1800 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
-          before-sleep 'swaylock -i /home/gabriel/Imagens/Illustrated-Celestial-Landscape-28.png'
